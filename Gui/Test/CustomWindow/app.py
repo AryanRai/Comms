92d7d6a3c8c9c2d046e8a7f52991173a4e844f23
aryanyan <arai0394@uni.sydney.edu.au>
2024-04-26 00:23:32 +1000

    Port to RPi4

--- Gui/Test/CustomWindow/app.py
+++ Gui/Test/CustomWindow/app.py
@@ -1,24 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtWebEngineWidgets import QWebEngineView
-import sys
-from PySide6.QtWidgets import QVBoxLayout
-from PySide6.QtCore import QUrl
-from PySide6.QtWebEngineWidgets import QWebEngineView
-# modify qevent code to work with PySide6
-from PySide6.QtCore import QEvent
 import webview
 
-class MyApp(QApplication):
-    def event(self, event: QEvent):
-        if event.type() == QEvent.Type.ApplicationActivate:
-            window.set_title("In focus")
-        if event.type() == QEvent.Type.ApplicationDeactivate:
-            window.set_title("Not in focus")
-        return super(QApplication, self).event(event)
-
-
-if __name__ == '__main__':
-    app = MyApp(sys.argv)
-    window = webview.create_window('Hello world', 'https://alvinwan.com/blog/')
-    webview.start(gui='qt')
+window = webview.create_window('Woah dude!', 'https://pywebview.flowrl.com')
+webview.start()
