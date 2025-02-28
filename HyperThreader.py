f2e9c709df5fc0d4b3c74a16c2b83fe845430ab9
aryanyan <31175254+AryanRai@users.noreply.github.com>
2025-03-01 00:40:24 +1100

    Revert V0.8.2
    AriesUI PreRel1.9.4: Implement AriesMods UI Only
    Cleanup Folder Structure

---
+++ HyperThreader.py
@@ -0,0 +1,7 @@
+import asyncio
+import subprocess
+
+#run stream handler
+subprocess.Popen(["python", "sh/stream_handler.py"])
+
+print("Stream handler running")
--- mainv2.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-import subprocess
-
-#run stream handler
-subprocess.Popen(["python", "StreamHandler/stream_handler.py"])
-
-print("Stream handler running")
